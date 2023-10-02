889aca2f24b890f7e48c3ec409b288a5aa5b3dbf
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-10-02 23:47:51 +0530

    JS : added booleans and string files

---
+++ JavaScript/boolean.js
@@ -0,0 +1,13 @@
+let a=2;
+let b=3;
+console.log(a>b);
+
+console.log(a<b);
+
+console.log(a==b);
+
+console.log(100=='100'); // == is used to compare the value only 
+console.log(100==='100');// === is used to compare the value and data type
+
+console.log(100!='100'); // != is used to compare the value only
+console.log(100!=='100');// !== is used to compare the value and data type
---
+++ JavaScript/string.js
@@ -0,0 +1,9 @@
+console.log('Hello World');
+let name = 'Dinesh';
+console.log('My name is ' + name);
+// Output:
+// Hello World
+// My name is Dinesh
+
+let adj = "It's a lovely day";
+console.log(adj);
