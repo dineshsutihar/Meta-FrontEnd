03aeb1bc4e813e9e91a26e245e59e32b80457ff5
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-10-02 23:49:38 +0530

    added some logical operators

--- JavaScript/boolean.js
+++ JavaScript/boolean.js
@@ -11,3 +11,26 @@ console.log(100==='100');// === is used to compare the value and data type
 
 console.log(100!='100'); // != is used to compare the value only
 console.log(100!=='100');// !== is used to compare the value and data type
+
+/*
+Logical AND operator: && 
+
+Logical OR operator: || 
+
+Logical NOT operator: ! 
+
+The modulus operator: % 
+
+The equality operator: ==
+
+The strict equality operator: ===
+
+The inequality operator: !=
+
+The strict inequality operator: !==
+
+The addition assignment operator: += 
+
+The concatenation assignment operator: += (it's the same as the previous one - more on that later
+
+ */
