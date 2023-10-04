dcacef75418951e0555b40602dac834158d778b8
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-10-04 13:19:52 +0530

    Js : Array

---
+++ JavaScript/array.js
@@ -0,0 +1,11 @@
+// Concept of Array In JavaScript
+var train =["Wheat","barley","red","green","yellow"];
+function arrList(train){
+    for(var i=0;i<train.length;i++){
+        console.log(train[i]);
+    }
+    console.log("Length of array is "+train.length);
+}
+arrList(train);
+console.log(train);
+
