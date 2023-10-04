5699bee6ce6228a7ce8587ac5aed430b103c29e5
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-10-04 23:41:32 +0530

    js : undefind, null, and empty

---
+++ JavaScript/NullUndefinedEmpty.js
@@ -0,0 +1,32 @@
+//Undefined, null and empty values
+//
+// Undefined
+// Undefined is a value that is automatically assigned to variables that have just been declared or to formal arguments for which there are no actual arguments.
+
+// var x;
+// console.log(x); //undefined
+
+// var y = 10;
+// console.log(y); //10
+
+// function add(a, b) {
+//     console.log(a + b);
+// }
+
+// add(10, 20); //30
+// add(10); //NaN
+// add(); //NaN
+
+// Null
+
+// Null is a value that is assigned to a variable to indicate that the variable has no value.
+
+// var x = null;
+// console.log(x); //null
+
+// var y = 10;
+// console.log(y); //10
+
+// function add(a, b) {
+//     console.log(a + b);
+// }
