0c8f8a4861492a17266aba75db0e6bebbd43fbba
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-10-04 23:06:43 +0530

    Js : Math Objects

---
+++ JavaScript/MathObject.js
@@ -0,0 +1,21 @@
+// Random methods of Math object
+
+// Math.random() method
+Math.random(); // returns a random number between 0 and 1
+
+//save the random number in a variable
+var randomNumber = Math.random();
+console.log(randomNumber);
+
+//multiply the random number by 10
+randomNumber = randomNumber * 10;
+console.log(randomNumber);
+
+//ceil the random number
+randomNumber = Math.ceil(randomNumber);
+console.log(randomNumber);
+
+
+// Math.floor() method
+randomNumber = Math.floor(3.2);
+console.log(randomNumber);
