3eba11640db6f909a9f01b30593b921b5b1bc6e2
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-10-07 23:42:14 +0530

    JS : Modules

---
+++ JavaScript/JS in the Browser/Modules/greetings.js
@@ -0,0 +1,13 @@
+export const informalGreeting = (person) => {
+    console.log(`Hi ${person}!`);
+};
+
+export const formalGreeting = (person) => {
+    console.log(`Greetings ${person}.`);
+};
+
+const greeting = () => {
+    console.log('Hello!');
+};
+
+export default greeting;
