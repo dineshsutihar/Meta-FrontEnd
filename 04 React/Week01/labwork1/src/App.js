0c7628c5c86c81df356bc4826ff8e0a1b9fdbcbc
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-03 11:46:32 +0530

    Add Testing component to App.js

--- 04 React/Week01/labwork1/src/App.js
+++ 04 React/Week01/labwork1/src/App.js
@@ -6,11 +6,26 @@ function Heading(){
   )
 }
 
+function Testing(){
+  return (
+    <div>
+      <h2>This is an h2 Heading</h2>
+      <h3>This is an h3 Heading</h3>
+      <h4>This is an h4 Heading</h4>
+      <h5>This is an h5 Heading</h5>
+      <h6>This is an h6 Heading</h6>
+     
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       This is the starting code for "labwork1" ungraded lab.
       <Heading />
+      <Testing />
+      
       
     </div>
   );
