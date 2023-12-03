76342118ed238ab7aa687f082b5811869e1fbc26
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-03 12:37:27 +0530

    Update App.js to include Nav component

--- 04 React/Week01/customizing-example/src/App.js
+++ 04 React/Week01/customizing-example/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Nav from './components/Nav.js';
+import Footer from './components';
+import Intro1 from './components';
+import Intro2 from './components';
+import Intro3 from './components';
+import Promo from './components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Nav />
       </header>
     </div>
   );
