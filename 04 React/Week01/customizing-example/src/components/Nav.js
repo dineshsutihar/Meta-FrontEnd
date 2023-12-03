b7dc5d3ecafcea49f69e618aae16e55354d33b65
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-03 12:02:23 +0530

    Add navigation component to the project

---
+++ 04 React/Week01/customizing-example/src/components/Nav.js
@@ -0,0 +1,12 @@
+function Nav() {
+    return (
+        <nav>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#about">About Me</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+        </nav>
+    )
+}
