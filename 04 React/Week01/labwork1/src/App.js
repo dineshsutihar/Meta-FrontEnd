57ffafdbf4c19fe66098ce88e5b4e399efedae15
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-03 11:39:46 +0530

    Remove logo and update App.js

--- 04 React/Week01/labwork1/src/App.js
+++ 04 React/Week01/labwork1/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Heading(){
+  return (
+    <h1>This is an h1 Heading</h1>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is the starting code for "labwork1" ungraded lab.
+      <Heading />
+      
     </div>
   );
 }
