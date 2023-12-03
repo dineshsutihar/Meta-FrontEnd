fc169c5ce053ad9e07d9622f8a5adaf685d0c2e2
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-03 12:09:15 +0530

    Add Intro1 component to customizing-example

---
+++ 04 React/Week01/customizing-example/src/components/Intro1.js
@@ -0,0 +1,13 @@
+function Intro1() {
+  return (
+    <div className="blog-post-intro">
+      <h2>I'va become a React developer!</h2>
+      <div>
+        <p>I've completed the React Basic course and I'm happy to announce that I'm now a Juniour React Developer!</p>
+        <p className="link">Read More...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Intro1;
