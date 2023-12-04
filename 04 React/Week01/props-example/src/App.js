87e7f9ac5f55a1dad6018c5ce9ca7d1419c73947
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-04 22:51:15 +0530

    Add components to App.js

--- 04 React/Week01/props-example/src/App.js
+++ 04 React/Week01/props-example/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Headers from './components/Headers';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
 
 
 function App() {
   return (
-    
+    <div>
+      <Headers />
+      <Main />
+      <Sidebar />
+    </div>
   );
 }
 
