f9fe24740bd61297678ea26f95ebfb3bc02901e2
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2023-12-04 22:56:52 +0530

    Update component import and props in App.js

--- 04 React/Week01/props-example/src/App.js
+++ 04 React/Week01/props-example/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Headers from './components/Headers';
+import Header from './components/Header';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 
@@ -7,7 +7,7 @@ import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div>
-      <Headers name="Anna" color="purple"/>
+      <Header name="Anna" color="purple"/>
       <Main greet="Dinesh" />
       <Sidebar greet="Hi" />
     </div>
